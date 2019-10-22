7f652c5626a705df227c77f41059c74ea7cb8504
jeffersonxu <me@jeffersonxu.com>
2019-10-21 23:57:45 -0400

    refactored ajax calls and favoriting function

--- favorite.js
+++ favorite.js
@@ -17,7 +17,8 @@ $('.list').on('click', '.favorite', function(event){
 });
 
 //Unfavorited
-$('.list', '#favorite').on('click', '.favorite_selected', function(event){
+//Case 1: Unfavoriting via Entires tab
+$('.list').on('click', '.favorite_selected', function(event){
     $(this).text('favorite_border');
     $(this).removeClass('favorite_selected');
     $(this).addClass('favorite');
@@ -36,10 +37,27 @@ $('.list', '#favorite').on('click', '.favorite_selected', function(event){
         $('.numFavorited').text(numFavorited + (numFavorited == 1 ? ' Entry' : ' Entries'));
 });
 
-//Case 2: Unfavoriting via Favorites tab 
-//Toggle favorite icon in Entries tab
+//Case 2: Unfavoriting via Favorites tab
 $('#favorite').on('click', '.favorite_selected', function(event){
+    $(this).text('favorite_border');
+    $(this).removeClass('favorite_selected');
+    $(this).addClass('favorite');
+
+    //delete card in Favorite tab
+    let id = $(this).attr('id').replace('fav-', '');
+    $('#favorite #' + id).remove();
+
+    //Toggle favorite icon in Entries tab
     $('#listWrapper ' + '#fav-' + id).text('favorite_border');
     $('#listWrapper ' + '#fav-' + id).removeClass('favorite_selected');
     $('#listWrapper ' + '#fav-' + id).addClass('favorite');
+
+    //Update Count
+    numFavorited--;
+    if(numFavorited == 0){
+        $('.numFavorited').empty();
+        $('no-fav').css('display', '');
+    }
+    else
+        $('.numFavorited').text(numFavorited + (numFavorited == 1 ? ' Entry' : ' Entries'));
 });
--- main.js
+++ main.js
@@ -9,6 +9,24 @@ $(document).ready(function(){
     $('.tabs').tabs();
 });
 
+//main runner
+$('.submit').click(function(){
+    $('.list').empty();
+    $('numList').empty();
+    let settings = {};
+    let category = $('#category').val();
+    settings.difficultyValue = $('#difficulty').val();
+    settings.startDate = moment('3/30/1964').format('YYYY-MM-DD');
+    settings.endDate = moment().format('YYYY-MM-DD');
+
+    if($('#startDate').val() !== '' && $('#endDate').val() !== ''){
+        settings.startDate = moment($('#startDate').val()).format('YYYY-MM-DD');
+        settings.endDate = moment($('#endDate').val()).format('YYYY-MM-DD');
+    }
+
+    search(category, 0, settings);
+});
+
 //helper functions
 function difficulty(num){
     if(num <= 333)
@@ -36,93 +54,105 @@ String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-//main runner
-$('.submit').click(function(){
-    $('.list').empty();
-    let category = $('#category').val();
-    let difficultyValue = $('#difficulty').val();
-    let startDate = moment('3/30/1964').format('YYYY-MM-DD');
-    let endDate = moment().format('YYYY-MM-DD');
+//recursive function that searches 100 entries (max capacity) at a time
+function search(category, offset, settings, notFound=true){
+    $.ajax({
+        url: url + 'categories',
+        data: {
+            "count" : 100,
+            "offset" : offset,
+        },
+        type: 'GET',
+        dataType: 'JSON',
+        success: function(categories){
+            //display error msg since no data found
+            if(categories == undefined || categories.length == 0){
+                $('.list').append(
+                    $('<div>', {class: 'row error red darken-4 white-text'}).append(
+                        $('<p>', {
+                            class: 'col s12',
+                            text: "Sorry we couldn't find any trivia questions that matched your search criteria. Try again?"
+                        }
+                    )).append(
+                        $('<i>', {
+                            class: 'medium material-icons',
+                            text: 'sentiment_very_dissatisfied'
+                })));
+            }
+            else {
+                //parse the 100 entries for category
+                categories.forEach(function(c){
+                    if(c.title !== null && (c.title.toLowerCase().includes(category.toLowerCase()))){
+                        notFound = false;
+                        $.ajax({
+                            url: url + 'clues',
+                            data: { "category" : c.id },
+                            type: 'GET',
+                            dataType: 'JSON',
+                            success: function(clues){
+                                //generate DOM elements for the clues found
+                                createList(clues, settings);
+                            }
+                        });
+                    }
+                });
+                //search another 100 entries
+                if(notFound)
+                    search(category, offset + 100, settings);
+            }
+        }
+    });
+}
 
-    if($('#startDate').val() !== '' && $('#endDate').val() !== ''){
-        startDate = moment($('#startDate').val()).format('YYYY-MM-DD');
-        endDate = moment($('#endDate').val()).format('YYYY-MM-DD');
-    }
-
-    search(category, difficultyValue, startDate, endDate);
-});
-
-function search(category, difficultyValue, startDate, endDate){
-    let bounds = getBounds(difficultyValue);
+//Creates DOM elements using Materialize components of the clues
+function createList(clues, settings){
+    let bounds = getBounds(settings.difficultyValue);
     let lower = bounds[0], upper = bounds[1];
 
-    $.ajax({
-        url: url + 'clues',
-        type: 'GET',
-        datatType: 'JSON',
-        success: function(data){
-            let count = 0;
-            data.forEach(function(element){
-                let correctCategory = element.category.title.toLowerCase().includes(category.toLowerCase());
-                let correctBounds = element.value >= lower && element.value <= upper;
-                let correctDate = moment(element.airdate).isBetween(startDate, endDate);
+    $('.numList').text(clues.length + (clues.length == 1 ? ' Entry' : ' Entries'));
+    clues.forEach(function(clue){
+        let correctDifficulty = clue.value >= lower && clue.value <= upper;
+        let correctDate = moment(clue.airdate).isBetween(settings.startDate, settings.endDate);
 
-                if(element.question !== "" && correctCategory && correctBounds && correctDate){
-                    let card = $('<div>', { class: 'card blue-grey darken-1'});
-                    let favorite = $('<a>').append()
-                    card.append(
-                        $('<div>', {class: 'card-content white-text'}).append(
-                            $('<span>', {
-                                class: 'card-title row',
-                                text: element.question
-                            }).append(
+        if(clue.question !== "" && correctDifficulty && correctDate){
+            let card = $('<div>', { class: 'card blue-grey darken-1'});
+            let favorite = $('<a>').append()
+            card.append(
+                $('<div>', {class: 'card-content white-text'}).append(
+                    $('<span>', {
+                        class: 'card-title row',
+                        text: clue.question
+                    }).append(
+                        $('<i>', {
+                            class: 'activator material-icons right white-text',
+                            text: 'more_vert'
+                            })).append(
+                            $('<a>').append(
                                 $('<i>', {
-                                    class: 'activator material-icons right white-text',
-                                    text: 'more_vert'
-                                    })).append(
-                                    $('<a>').append(
-                                        $('<i>', {
-                                            id: 'fav-' + element.id,
-                                            class: 'right material-icons favorite',
-                                            text: 'favorite_border'
-                                        })))).append(
-                                    $('<p>', {
-                                        text: 'Aired: ' + moment(element.airdate).format('MMMM Do, YYYY')
-                                    })));
+                                    id: 'fav-' + clue.id,
+                                    class: 'right material-icons favorite',
+                                    text: 'favorite_border'
+                                })))).append(
+                            $('<p>', {
+                                text: 'Aired: ' + moment(clue.airdate).format('MMMM Do, YYYY')
+            })));
 
-                    card.append(
-                        $('<div>', { class: 'card-reveal'}).append(
-                            $('<span>', {
-                                class: 'card-title',
-                                text: 'Answer: ' + element.answer.replace('<i>', '').replace('</i>', '').capitalize()
-                            }).append(
-                                $('<i>', {
-                                    class: 'material-icons right',
-                                    text: 'close'
-                                }))
-                            ).append(
-                                $('<p>', {text: 'Difficulty: ' + difficulty(element.value)} ).append(
-                                    $('<span>', {class: 'category', text: 'Category: ' + element.category.title.capitalize()}).append(
-                                        ))));
+            card.append(
+                $('<div>', { class: 'card-reveal'}).append(
+                    $('<span>', {
+                        class: 'card-title',
+                        text: 'Answer: ' + clue.answer.replace('<i>', '').replace('</i>', '').capitalize()
+                    }).append(
+                        $('<i>', {
+                            class: 'material-icons right',
+                            text: 'close'
+                        }))
+                    ).append(
+                        $('<p>', {text: 'Difficulty: ' + difficulty(clue.value)} ).append(
+                            $('<span>', {class: 'category', text: 'Category: ' + clue.category.title.capitalize()}).append(
+                                ))));
 
-                    $('.list').append($('<div>', { id: element.id, class: 'row' }).append(card));
-                    count++;
-                }
-        });
-
-        if(count === 0){
-            $('.list').append(
-                $('<div>', {class: 'row error red darken-4 white-text'}).append(
-                    $('<p>', {
-                        class: 'col s12',
-                        text: "Sorry we couldn't find any trivia questions that matched your search criteria. Try again?"
-                    }
-                )).append(
-                    $('<i>', {
-                        class: 'medium material-icons',
-                        text: 'sentiment_very_dissatisfied'
-                    })));
-        } else
-            $('.numList').text(count + (count == 1 ? ' Entry' : ' Entries'));
-    }});
+            $('.list').append($('<div>', { id: clue.id, class: 'row' }).append(card));
+        }
+    });
 }
